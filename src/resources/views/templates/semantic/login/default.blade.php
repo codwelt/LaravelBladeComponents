8e4b53895da384d397335c864497024736594cb3
FuriosoJack <iam@furiosojack.com>
2019-05-12 12:31:16 -0500

    Se añade template completo de login

--- src/resources/views/templates/semantic/login/default.blade.php
+++ src/resources/views/templates/semantic/login/default.blade.php
@@ -1,30 +1,55 @@
-<div class="ui middle aligned center aligned grid">
-    <div class="column">
-        <h2 class="ui orange image header">
-            <img src="" class="image">
-            <div class="content">
-                Autenticacion Administrador
-            </div>
-        </h2>
-        @include(\Codwelt\LaravelBladeComponents\LaravelBladeComponentsProvider::NAMESPACE_PROYECT . "::templates.semantic.errorsValidation")
-        <form class="ui large form" action="{{$action}}" method="POST">
-            @csrf
-            <div class="ui stacked segment">
-                <div class="field">
-                    <div class="ui left icon input">
-                        <i class="user icon"></i>
-                        <input type="text" name="username" placeholder="UserName">
-                    </div>
-                </div>
-                <div class="field">
-                    <div class="ui left icon input">
-                        <i class="lock icon"></i>
-                        <input type="password" name="password" placeholder="Password">
-                    </div>
-                </div>
-                <button class="ui fluid large orange submit button" type="submit">@lang(\Codwelt\LaravelBladeComponents\LaravelBladeComponentsProvider::NAMESPACE_PROYECT . "::login.submit")</button>
-            </div>
-        </form>
+@extends("laravel_blade_components::templates.semantic.skeleton")
+@section("title",config('app.name',"Comidas Rapidas"))
 
-    </div>
-</div>
+@section("header")
+    <style type="text/css">
+        body {
+            background-color: #DADADA;
+        }
+        body > .grid {
+            height: 100%;
+        }
+        .image {
+            margin-top: -100px;
+        }
+        .column {
+            max-width: 450px;
+        }
+    </style>
+@endsection
+@section('body')
+    <div class="ui container">
+        <div class="ui middle aligned center aligned grid">
+            <div class="column">
+                <h2 class="ui orange image header">
+                    <img src="" class="image">
+                    <div class="content">
+                        Autenticacion Administrador
+                    </div>
+                </h2>
+                @include(\Codwelt\LaravelBladeComponents\LaravelBladeComponentsProvider::NAMESPACE_PROYECT . "::templates.semantic.errorsValidation")
+                <form class="ui large form" action="{{$action}}" method="POST">
+                    @csrf
+                    <div class="ui stacked segment">
+                        <div class="field">
+                            <div class="ui left icon input">
+                                <i class="user icon"></i>
+                                <input type="text" name="username" placeholder="UserName">
+                            </div>
+                        </div>
+                        <div class="field">
+                            <div class="ui left icon input">
+                                <i class="lock icon"></i>
+                                <input type="password" name="password" placeholder="Password">
+                            </div>
+                        </div>
+                        <button class="ui fluid large orange submit button" type="submit">@lang(\Codwelt\LaravelBladeComponents\LaravelBladeComponentsProvider::NAMESPACE_PROYECT . "::login.submit")</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+
+@endsection
+
+
--- src/resources/views/templates/semantic/skeleton.blade.php
+++ src/resources/views/templates/semantic/skeleton.blade.php
@@ -4,14 +4,33 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
-
-
     <link href="{{laravel_blade_components_asset('/semantic/semantic.css')}}" rel="stylesheet">
     <script src="{{laravel_blade_components_asset('js/jquery-3.3.1.min.js')}}"></script>
     <script src="{{laravel_blade_components_asset('/semantic/semantic.js')}}"></script>
-</head>
+
+    @yield("header")
 </head>
 <body>
 @yield('body')
+
+<script type="text/javascript">
+
+    //Efecto de quitar para mensajes de error
+    $('.message .close')
+        .on('click', function() {
+            $(this)
+                .closest('.message')
+                .transition('fade')
+            ;
+        });
+
+    //Incializa Dropdowns
+    $('.ui.dropdown').dropdown();
+
+    $('.ui.checkbox').checkbox();
+</script>
+@yield('scripts')
+
+
 </body>
 </html>
